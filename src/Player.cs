05bf7682e67b6900e420ab438caf2b5f073f1d3d
Andrew <androw.emad@yahoo.com>
2022-11-21 18:41:47 +0200

    player is dead when his health reaches 0

--- src/Player.cs
+++ src/Player.cs
@@ -8,12 +8,13 @@ namespace No_Ghosts
 {
     public abstract class Player : GameObject, IDrawable
     {
-        public Texture2D texture {get; set;}
+        public Texture2D aliveTexture {get; set;}
         public Vector2 textureScale{get; set;}
         public Single angle {get; set;}
         public Color textureColor{get; set;}
         public CollisionBehavior collisionBehavior;
         public Vector2 shootPoint;
+        public int health{get; set;} = 100;
         public int bulletsInMagazine{get; protected set;} = 30;
         public abstract void draw(ref SpriteBatch spriteBatch);
         public abstract void loadContent(ContentManager manager);
@@ -24,10 +25,10 @@ namespace No_Ghosts
     }
     public class GrayPlayer : Player
     {
-        private const float GUN_SHOOTING_SPEED = 0.2f;
+        private const float GUN_SHOOTING_SPEED = 0.13f;
         private const float GUN_RELOAD_TIME = 1f;
-        private float lastGunShotTime;
-        private float reloadTime;
+        private float timePassedForLastGunShot;
+        private float timePassedToReload;
         public Texture2D currentTexture;
         public SpriteEffects textureEffect;
         Texture2D deadTexture;
@@ -42,31 +43,38 @@ namespace No_Ghosts
         }
         public override void update(GameTime gameTime, GameFactory factory, KeyboardState keyboardState, MouseState mouse, GraphicsDeviceManager _graphics)
         {
+            if(health == 0)
+            {
+                this.currentTexture = this.deadTexture;
+                return;
+            }
             if (keyboardState.IsKeyDown(Keys.A))
                 this.moveLeft(gameTime, _graphics);
             if (keyboardState.IsKeyDown(Keys.D))
                 this.moveRight(gameTime, _graphics);
             this.changeTextureAccordingToMousePosition(mouse);
-            lastGunShotTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            timePassedForLastGunShot += (float)gameTime.ElapsedGameTime.TotalSeconds;
         }
         public override Projectile shoot(GameTime gameTime, GameFactory factory, MouseState mouse)
         {
+            if(this.health == 0)
+                return null;
             shootPoint = new Vector2(this.position.X, this.position.Y);
             if(mouse.LeftButton == ButtonState.Pressed)
             {
-                if (lastGunShotTime >= GrayPlayer.GUN_SHOOTING_SPEED && bulletsInMagazine > 0)
+                if (timePassedForLastGunShot >= GrayPlayer.GUN_SHOOTING_SPEED && bulletsInMagazine > 0)
                 {
-                    lastGunShotTime = 0;
+                    timePassedForLastGunShot = 0;
                     bulletsInMagazine -= 1;
                     return factory.CreateBullet(BulletType.PlayerRegularBullet, shootPoint, new Vector2(mouse.X, mouse.Y));
                 }
             }
             if(bulletsInMagazine == 0)
             {
-                reloadTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (reloadTime >= GrayPlayer.GUN_RELOAD_TIME)
+                timePassedToReload += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (timePassedToReload >= GrayPlayer.GUN_RELOAD_TIME)
                 {
-                    reloadTime = 0;
+                    timePassedToReload = 0;
                     bulletsInMagazine = 30;
                 }
             }
@@ -74,9 +82,9 @@ namespace No_Ghosts
         }
         public override void loadContent(ContentManager manager)
         {
-            this.texture = manager.Load<Texture2D>("Textures/PlayerTextures/GrayPlayerAlive");
+            this.aliveTexture = manager.Load<Texture2D>("Textures/PlayerTextures/GrayPlayerAlive");
             this.deadTexture = manager.Load<Texture2D>("Textures/PlayerTextures/GrayPlayerDead");
-            currentTexture = texture;
+            currentTexture = aliveTexture;
             collisionBehavior.setRectangleAttrs(new Vector2(
                 position.X - (currentTexture.Width * textureScale.X) / 2,
                 position.Y - (currentTexture.Height * textureScale.Y) / 2
@@ -89,7 +97,7 @@ namespace No_Ghosts
         {
             float distanceToTravel = speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             if(
-                position.X + distanceToTravel + (texture.Width * textureScale.X / 2) < graphics.PreferredBackBufferWidth 
+                position.X + distanceToTravel + (aliveTexture.Width * textureScale.X / 2) < graphics.PreferredBackBufferWidth 
                 //&& position.X + distanceToTravel >= (texture.Width * textureScale.X / 2)
             )
             {
@@ -106,7 +114,7 @@ namespace No_Ghosts
             float distanceToTravel = speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             if(
                 //position.X + distanceToTravel <= graphics.PreferredBackBufferWidth + (texture.Width * textureScale.Y / 2) &&
-                position.X + distanceToTravel >= (texture.Width * textureScale.Y / 2)
+                position.X + distanceToTravel >= (aliveTexture.Width * textureScale.Y / 2)
             )
             {
                 this.position.X -= speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -130,7 +138,7 @@ namespace No_Ghosts
         }
         public override void draw(ref SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.texture, position, null, Color.White, angle, new Vector2(texture.Width / 2, texture.Height / 2), textureScale, this.textureEffect, 0f);
+            spriteBatch.Draw(this.currentTexture, position, null, new Color(255, health+155, health+155), angle, new Vector2(aliveTexture.Width / 2, aliveTexture.Height / 2), textureScale, this.textureEffect, 0f);
         }
     }
 }
